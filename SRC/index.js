de8ccfca273d355d09fd54014e40f01bbba79bf2
AHernandez810 <ashleyhernandez810.ah@gmail.com>
2020-10-11 15:07:07 -0400

    added innerhtml elements

--- SRC/index.js
+++ SRC/index.js
@@ -34,7 +34,19 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
-  let iconElement = document.querySelector("weather-icon");
+  let iconElement = document.querySelector("#weather-icon");
 
   celsiusTemperature = response.data.main.temp;
+
+  tempElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `https://cdn0.iconfinder.com/data/icons/${response.data.weather[0].icon}weather-filled-outline-6/64/weather_cloud_sun_moon_rain-27-512.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
