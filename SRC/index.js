566800d398f98bf24153bc714305fab13b02b1b6
AHernandez810 <ashleyhernandez810.ah@gmail.com>
2020-10-12 00:26:09 -0400

    fixed humidity and wind elements

--- SRC/index.js
+++ SRC/index.js
@@ -22,11 +22,16 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let locationElement = document.querySelector("#location");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
 
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   locationElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
 }
 
 let apiKey = "8542b50982e200d0127f4bfc3b2ce168";
